2107a505227b3531626b654438047398805e0dfb
rmit-s3579161-loso-lai <s3579161@student.rmit.edu.au>
2017-09-14 15:24:40 +1000

    lab6

---
+++ src/Week07_Sol/BubbleSort1.java
@@ -0,0 +1,31 @@
+package Week07_Sol;
+import java.util.ArrayList;
+
+/**
+ * Standard, no early termination, bubble sort.
+ * 
+ * @author jefcha
+ */
+public class BubbleSort1 extends SortAlgorithm
+{
+    /**
+     * Sorts the input array.
+     * 
+     * @param array Array to be sorted.
+     */
+    public void sort(int[] array) {
+        
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                // check if we need to swap
+                if (array[j] > array[j+1]) {
+                    Integer temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+        
+    } // end of sort()
+    
+} // end of class BubbleSort1
